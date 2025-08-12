901650ea03b5447f2bb4e2b5808cb4c8207acfc5
misbashaikh2004 <misba6602@gmail.com>
2025-08-12 15:27:30 +0530

    First commit

---
+++ maxno.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],n,i,max;
+    printf("enter n:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+max=a[0];
+for(i=1;i<n;i++)
+{
+    if(max<a[i])
+    max=a[i];
+}
+printf("maximum is %d",max);
+return 0;
+}

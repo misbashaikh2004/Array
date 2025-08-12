391c6f3a45d5dbc4473708751a8f28a115ecb940
misbashaikh2004 <misba6602@gmail.com>
2025-08-12 15:03:19 +0530

    First commit

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],n,i;
+    printf("enter a no:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+
+    printf("given data is \n");
+    for(i=0;i<n;i++)
+
+    printf("%d\n",a[i]);
+    return 0;
+}
